0ec64024f298c51a40c9ac0e217561065e80e8e1
Radina Avramova <99686592+RadinaAvramova@users.noreply.github.com>
2024-05-18 16:33:15 +0300

    Add files via upload

---
+++ C# Advanced/C# Advanced - Exams/C# Advanced Retake Exam - 13 December 2023/03. Groceries Management_Author Solution/GroceriesStore.cs
@@ -0,0 +1,65 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace GroceriesManagement
+{
+    public class GroceriesStore
+    {
+        public GroceriesStore(int capacity)
+        {
+            Capacity = capacity;
+            Turnover = 0;
+            Stall = new List<Product>();
+        }
+
+        public int Capacity { get; set; }
+
+        public double Turnover { get; set; }
+
+        public List<Product> Stall { get; set; }
+
+
+        public void AddProduct(Product product)
+        {
+            if(Stall.Contains(product) || Stall.Count == Capacity)
+            {
+                return;
+            }
+            Stall.Add(product);
+        }
+
+        public bool RemoveProduct(string name) => Stall.Remove(Stall.FirstOrDefault(x => x.Name == name));
+
+        public string SellProduct(string name, double quantity)
+        {
+            if(!Stall.Any(x => x.Name == name))
+            {
+                return $"Product not found";
+            }
+
+            Product product = Stall.First(x => x.Name == name);
+            double totalPrice = Math.Round(product.Price * quantity, 2);
+            Turnover += totalPrice;
+
+            return $"{product.Name} - {totalPrice:f2}$";
+        }
+
+        public string GetMostExpensive() => Stall.OrderByDescending(x => x.Price).FirstOrDefault().ToString();
+
+        public string CashReport() => $"Total Turnover: {Turnover:f2}$";
+
+        public string PriceList()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Groceries Price List:");
+
+            foreach(Product product in Stall)
+            {
+                sb.AppendLine(product.ToString());
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
