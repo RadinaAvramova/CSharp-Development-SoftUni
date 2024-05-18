fd0d0fd721e6fccf316837edfb9ce25f18360ec6
Radina Avramova <99686592+RadinaAvramova@users.noreply.github.com>
2024-05-18 16:22:50 +0300

    Add files via upload

---
+++ C# Advanced/C# Advanced - Exams/C# Advanced Regular Exam - 21 October 2023/03. Mail Client_Author Solution/MailBox.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+
+namespace MailClient
+{
+    public class MailBox
+    {
+        public MailBox(int capacity)
+        {
+            Capacity = capacity;
+            Inbox = new List<Mail>();
+            Archive = new List<Mail>();
+        }
+
+        public int Capacity { get; set; }
+
+        public List<Mail> Inbox { get; set; }
+
+        public List<Mail> Archive { get; set; }
+
+
+        public void IncomingMail(Mail mail)
+        {
+            if (Inbox.Count < Capacity) 
+            {
+                Inbox.Add(mail);
+            }
+        }
+
+        public bool DeleteMail(string sender)
+            => Inbox.Remove(Inbox.FirstOrDefault(m => m.Sender == sender));
+
+        public int ArchiveInboxMessages()
+        {
+            int count = Inbox.Count;
+            Archive.AddRange(Inbox);
+            Inbox = new List<Mail>();
+
+            return count;
+        }
+
+        public string GetLongestMessage()
+            => Inbox.OrderByDescending(m => m.Body.Length).FirstOrDefault().ToString();
+
+        public string InboxView()
+        {
+            StringBuilder sb = new ();
+            sb.AppendLine("Inbox:");
+
+            foreach (Mail mail in Inbox)
+            {
+                sb.AppendLine(mail.ToString().TrimEnd());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
