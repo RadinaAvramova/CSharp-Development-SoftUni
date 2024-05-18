cacdac6253120ab3a644463e88897846e8335d03
Radina Avramova <99686592+RadinaAvramova@users.noreply.github.com>
2024-05-18 16:37:43 +0300

    Add files via upload

---
+++ C# Advanced/C# Advanced - Exams/C# Advanced Regular Exam - 17 February 2024/03. Shark Taxonomy_Author Solution/Classifier.cs
@@ -0,0 +1,49 @@
+﻿using System.Dynamic;
+using System.Text;
+
+namespace SharkTaxonomy
+{
+    public class Classifier
+    {
+        public Classifier(int capacity)
+        {
+            Capacity = capacity;
+            Species = new List<Shark>();
+        }
+
+        public int Capacity { get; set; }
+
+        public List<Shark> Species { get; set; }
+
+        public int GetCount => Species.Count;
+
+        public void AddShark(Shark s)
+        {
+            if(Capacity == Species.Count || Species.Any(shark => shark.Kind == s.Kind))
+            {
+                return;
+            }
+            Species.Add(s);
+        }
+
+        public bool RemoveShark(string kind) => Species.Remove(Species.FirstOrDefault(s => s.Kind == kind));
+
+        public string GetLargestShark() => Species.OrderByDescending(s => s.Length).FirstOrDefault().ToString();
+
+        public double GetAverageLength() => Species.Average(s => s.Length);
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"{GetCount} sharks classified:");
+
+            foreach(Shark s in Species )
+            {
+                sb.AppendLine(s.ToString().Trim());
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ C# Advanced/C# Advanced - Exams/C# Advanced Regular Exam - 17 February 2024/SharkTaxonomy-Skeleton/SharkTaxonomy/Classifier.cs
@@ -0,0 +1,7 @@
+﻿namespace SharkTaxonomy
+{
+    public class Classifier
+    {
+
+    }
+}
