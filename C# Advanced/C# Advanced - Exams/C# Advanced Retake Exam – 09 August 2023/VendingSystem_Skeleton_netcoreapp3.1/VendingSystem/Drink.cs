092c119a53bcf162c97734d56df7e5924d0acd6e
Radina Avramova <99686592+RadinaAvramova@users.noreply.github.com>
2024-05-18 16:18:38 +0300

    Add files via upload

---
+++ C# Advanced/C# Advanced - Exams/C# Advanced Retake Exam – 09 August 2023/VendingSystem_Skeleton_netcoreapp3.1/VendingSystem/Drink.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VendingSystem
+{
+    public class Drink
+    {
+        
+    }
+}
---
+++ C# Advanced/C# Advanced - Exams/C# Advanced Retake Exam – 09 August 2023/VendingSystem_Skeleton_netcoreapp6.0/VendingSystem/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VendingSystem
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
