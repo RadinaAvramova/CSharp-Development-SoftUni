90bbb72420eaf1cf931adc1a18d594c15fa02da3
Radina Avramova <99686592+RadinaAvramova@users.noreply.github.com>
2024-05-21 08:28:00 +0300

    Add files via upload

---
+++ C# Advanced/C# Advanced - Exams/C# Advanced Regular Exam - 17 June 2023/03. Automotive Repair Shop_Author Solution/RepairShop.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+namespace AutomotiveRepairShop
+{
+    public class RepairShop
+    {
+        public RepairShop(int capacity)
+        {
+            Capacity = capacity;
+            this.Vehicles = new List<Vehicle>();
+        }
+
+        public int Capacity { get; set; }
+
+        public List<Vehicle> Vehicles { get; set; }
+
+        public void AddVehicle(Vehicle vehicle)
+        {
+            if(this.Vehicles.Count < this.Capacity)
+            {
+                this.Vehicles.Add(vehicle);
+            }
+        }
+
+        public bool RemoveVehicle(string vin) => this.Vehicles.Remove(this.Vehicles.FirstOrDefault(v => v.VIN == vin));
+
+        public int GetCount() => this.Vehicles.Count;
+
+        public Vehicle GetLowestMileage() => this.Vehicles.OrderBy(v => v.Mileage).FirstOrDefault();
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Vehicles in the preparatory:");
+
+            foreach(Vehicle v in this.Vehicles)
+            {
+                sb.AppendLine(v.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# Advanced/C# Advanced - Exams/C# Advanced Regular Exam - 17 June 2023/03. Automotive Repair Shop_Author Solution/Vehicle.cs
@@ -0,0 +1,22 @@
+﻿namespace AutomotiveRepairShop
+{
+    public class Vehicle
+    {
+        public Vehicle(string vin, int mileage, string damage)
+        {
+            VIN = vin;
+            Mileage = mileage;
+            Damage = damage;
+        }
+
+        public string VIN { get; set; }
+        public int Mileage { get; set; }
+
+        public string Damage { get; set; }
+
+        public override string ToString()
+        {
+            return $"Damage: {this.Damage}, Vehicle: {this.VIN} ({this.Mileage} km)";
+        }
+    }
+}
